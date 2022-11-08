301b815107c6b3d71a8042c46d7b55d4376be6f0
jshelver <j.r.shelver@gmail.com>
2022-11-08 17:47:22 -0600

    Set Up Basic Lobby

---
+++ Assets/Scripts/GameNetworkManager.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Netcode.Transports.Facepunch;
+using Steamworks;
+using Steamworks.Data;
+using Unity.Netcode;
+
+public class GameNetworkManager : MonoBehaviour
+{
+    public static GameNetworkManager instance;
+
+    public Lobby? currentLobby = null;
+
+    FacepunchTransport transport;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        transport = GetComponent<FacepunchTransport>();
+
+        SteamMatchmaking.OnLobbyCreated += OnLobbyCreated;
+        SteamMatchmaking.OnLobbyEntered += OnLobbyEntered;
+        SteamMatchmaking.OnLobbyMemberJoined += OnLobbyMemberJoined;
+        SteamMatchmaking.OnLobbyMemberLeave += OnLobbyMemberLeave;
+        SteamMatchmaking.OnLobbyInvite += OnLobbyInvite;
+        SteamMatchmaking.OnLobbyGameCreated += OnLobbyGameCreated;
+        SteamFriends.OnGameLobbyJoinRequested += OnGameLobbyJoinRequested;
+    }
+
+    void OnDestroy()
+    {
+        SteamMatchmaking.OnLobbyCreated -= OnLobbyCreated;
+        SteamMatchmaking.OnLobbyEntered -= OnLobbyEntered;
+        SteamMatchmaking.OnLobbyMemberJoined -= OnLobbyMemberJoined;
+        SteamMatchmaking.OnLobbyMemberLeave -= OnLobbyMemberLeave;
+        SteamMatchmaking.OnLobbyInvite -= OnLobbyInvite;
+        SteamMatchmaking.OnLobbyGameCreated -= OnLobbyGameCreated;
+        SteamFriends.OnGameLobbyJoinRequested += OnGameLobbyJoinRequested;
+
+        if (NetworkManager.Singleton == null) return;
+
+        NetworkManager.Singleton.OnServerStarted -= OnServerStarted;
+        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnect;
+    }
+
+    void OnApplicationQuit()
+    {
+        Disconnect();
+    }
+
+    public async void StartHost(int maxClients = 100)
+    {
+        NetworkManager.Singleton.OnServerStarted += OnServerStarted;
+
+        if (NetworkManager.Singleton.StartHost())
+            Debug.Log("Host Started");
+        else
+            Debug.Log("Host Failed to Start");
+
+        await SteamMatchmaking.CreateLobbyAsync(maxClients);
+    }
+
+    public void StartClient(SteamId steamId)
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnect;
+
+        transport.targetSteamId = steamId;
+
+        if (NetworkManager.Singleton.StartClient())
+            Debug.Log("Client Started");
+        else
+            Debug.Log("Client Failed to Start");
+    }
+
+    public void Disconnect()
+    {
+        currentLobby?.Leave();
+
+        if (NetworkManager.Singleton == null) return;
+
+        NetworkManager.Singleton.Shutdown();
+    }
+
+    #region Steam Callbacks
+
+    private void OnLobbyCreated(Result result, Lobby lobby)
+    {
+        if (result != Result.OK)
+        {
+            Debug.LogError($"Failed to create lobby: {result}", this);
+            UIManager.instance.LobbyCreationFailed();
+            return;
+        }
+
+        string lobbyName = lobby.Owner.Name + "'s Lobby";
+
+        lobby.SetFriendsOnly();
+        lobby.SetData("name", lobbyName);
+        lobby.SetJoinable(true);
+
+        UIManager.instance.SetLobbyTitle(lobbyName);
+        UIManager.instance.UpdateMemberList(lobby.Members);
+        UIManager.instance.SetLobbyTitle(lobbyName);
+        UIManager.instance.UpdateMemberList(lobby.Members);
+
+        currentLobby = lobby;
+        Debug.Log($"Lobby Created: {lobby.Id}", this);
+    }
+
+    private void OnLobbyEntered(Lobby lobby)
+    {
+        if (NetworkManager.Singleton.IsHost) return;
+
+        string lobbyName = lobby.GetData("name");
+
+        UIManager.instance.SetLobbyTitle(lobbyName);
+        UIManager.instance.UpdateMemberList(lobby.Members);
+
+        StartClient(lobby.Id);
+    }
+
+    private void OnLobbyMemberJoined(Lobby lobby, Friend friend) { }
+
+    private void OnLobbyMemberLeave(Lobby lobby, Friend friend) { }
+
+    private void OnLobbyInvite(Friend friend, Lobby lobby) => Debug.Log($"You got a invite from {friend.Name}", this);
+
+    private void OnLobbyGameCreated(Lobby lobby, uint value, ushort value2, SteamId steamId) { }
+
+    private void OnGameLobbyJoinRequested(Lobby lobby, SteamId steamId)
+    {
+        StartClient(lobby.Id);
+    }
+
+    #endregion
+
+    #region Network Callbacks
+
+    private void OnServerStarted() => Debug.Log("Server Started", this);
+
+    private void OnClientConnected(ulong clientId) => Debug.Log($"Client Connected, clientId: {clientId}");
+
+    private void OnClientDisconnect(ulong clientId)
+    {
+        Debug.Log($"Client Disconnected, clientId: {clientId}");
+
+        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnect;
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Netcode.Transports.Facepunch;
+using Steamworks;
+using Steamworks.Data;
+using Unity.Netcode;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [Header("References")]
+    [SerializeField] GameObject startHostButton;
+    [SerializeField] GameObject lobbyMenu;
+    [SerializeField] TextMeshProUGUI lobbyTitleText;
+    [SerializeField] GameObject playerListItem;
+    [SerializeField] GameObject playerListHolder;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        lobbyMenu.SetActive(false);
+        startHostButton.SetActive(true);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void StartHost(int maxClients)
+    {
+        startHostButton.SetActive(false);
+        GameNetworkManager.instance.StartHost(maxClients);
+    }
+
+    public void LobbyCreationFailed()
+    {
+        startHostButton.SetActive(true);
+    }
+
+    public void SetLobbyTitle(string title)
+    {
+        lobbyMenu.SetActive(true);
+        lobbyTitleText.text = string.IsNullOrEmpty(title) ? "Lobby" : title;
+    }
+
+    public void LeaveLobby()
+    {
+        GameNetworkManager.instance.Disconnect();
+        lobbyMenu.SetActive(false);
+        startHostButton.SetActive(true);
+    }
+
+    public void UpdateMemberList(IEnumerable<Friend> members)
+    {
+        foreach (Transform child in playerListHolder.transform)
+        {
+            // Destroy all old player list items
+            if (child.gameObject.GetComponent<PlayerListItemData>() != null)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        // Create new player list items for all members
+        foreach (Friend member in members)
+        {
+            AddPlayerToList(member.Name);
+        }
+    }
+
+    private void AddPlayerToList(string playerName)
+    {
+        PlayerListItemData newPlayer = Instantiate(playerListItem, playerListHolder.transform).GetComponent<PlayerListItemData>();
+        newPlayer.transform.SetAsLastSibling();
+        newPlayer.playerNameText.text = playerName;
+    }
+}
